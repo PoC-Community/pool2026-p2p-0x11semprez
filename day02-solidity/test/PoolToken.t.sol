72750670e1918c41aca2255cd685697f07e3427a
0x11semprez <traoresemprez@icloud.com>
2026-02-03 14:33:08 +0100

    feat: ok

---
+++ day02-solidity/test/PoolToken.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.3;
+
+import {Test} from "forge-std/Test.sol";
+import {PoolToken} from "../src/PoolToken.sol";
+
+contract TestPoolToken is Test {
+    PoolToken public token;
+
+    address owner = address(this);
+    address user = 0x7585149f7a1281280fe44Aff1c6055623B44f395;
+
+    uint constant INITIAL_SUPPLY = 100000000 ether;
+
+    function setUp() public {
+        token = new PoolToken(INITIAL_SUPPLY);
+    }
+
+    function testInitialSupply() public {
+        assertEq(token.totalSupply(), INITIAL_SUPPLY);
+        assertEq(token.balanceOf(owner), INITIAL_SUPPLY);
+    }
+
+    function testOnlyOwnerCanMint() public {
+        vm.prank(user);
+        vm.expectRevert();
+        token.mint(user, 1000 ether);
+    }
+}
