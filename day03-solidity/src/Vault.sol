1f2aafdda67b77dfd5199a069ec4da466cf9e4bc
0x11semprez <traoresemprez@icloud.com>
2026-02-04 15:13:26 +0100

    feat: test3 doesn't pass because I need  mock but all is right

--- day03-solidity/src/Vault.sol
+++ day03-solidity/src/Vault.sol
@@ -6,13 +6,14 @@ import "@solady/src/utils/SafeTransferLib.sol";
 import "@solady/src/utils/ReentrancyGuard.sol";
 import "@solady/src/auth/Ownable.sol";
 
-contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
+contract Vault is ReentrancyGuard, Ownable {
     error ZeroAssets(); //keccak256 0x1f2a2005cb66a8e145327e8814e243a0996aec9bfe1e15a495778b1236dbd485
     error InsufficientShares(); //keccak26 0x399965675cfec4301cbe5ec24fb407575c5a7e4f40d219532068c8e5b35040f9
     error ZeroShares(); //keccak256 0x9811e0c7eeca2ee0fa751e2e19d78682ad689ea04816cfe0cf1c5863234abd3f
 
     event Deposit(address indexed user, uint asset, uint shares);
     event Withdraw(address indexed user, uint256 assets, uint256 shares);
+    event RewardAdded(uint amout);
 
     using SafeTransferLib for address;
 
@@ -25,13 +26,15 @@ contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
         ASSET = _asset;
     }
 
+    function getAssetBalance() internal view returns (uint256) {
+        return ASSET.balanceOf(address(this));
+    }
+ 
+
     function getAsset() external view returns (address) {
         return address(ASSET);
     }
 
-    function getAssetBalance() internal view returns (uint256) {
-        return ASSET.balanceOf(address(this));
-    }
 
     function getAssetDecimals() internal view returns (uint256) {
         return ASSET.decimals();
@@ -46,10 +49,26 @@ contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
         }
     }
 
+    function currentRatio() external view returns (uint ratio) {
+      assembly {
+        let _totalShares := sload(totalShares.slot)
+        if iszero(_totalShares) {
+          ratio := 1000000000000000000
+          mstore(0x00, ratio)
+          return(0x00, 32)
+        }
+      }
+      if (ratio != 0) return ratio;
+
+      uint totalAsset = getAssetBalance();
+
+      ratio = totalAsset * 1e18/totalShares;
+    }
+
     function _convertToShares(
-        uint256 asset
-    ) internal view returns (uint256 shares) {
-        uint256 _totalAssets = getAssetBalance();
+        uint asset
+    ) internal view returns (uint shares) {
+        uint _totalAssets = getAssetBalance();
 
         if (totalShares == 0) {
             return asset;
@@ -59,9 +78,9 @@ contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
     }
 
     function _convertToAssets(
-        uint256 shares
-    ) internal view returns (uint256 asset) {
-        uint256 _totalAssets = getAssetBalance();
+        uint  shares
+    ) internal view returns (uint asset) {
+        uint  _totalAssets = getAssetBalance();
 
         if (totalShares == 0) {
             return 0;
@@ -69,6 +88,14 @@ contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
 
         return (shares * _totalAssets) / totalShares;
     }
+
+    function assetOf(address user) external view returns(uint totalAssetUser, uint totalSharesUser) {
+      totalSharesUser = sharesOf[user];
+
+      totalAssetUser = _convertToAssets(totalSharesUser);
+    } 
+
+
 
     function deposit(uint assets) external nonReentrant returns (uint shares) {
         assembly {
@@ -83,18 +110,21 @@ contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
 
         address from = msg.sender;
         sharesOf[from] += shares;
-        shares += totalShares;
+        totalShares += shares;
 
         address to = address(this);
-        assets.SafeTransferFrom(from, to, assets);
+        address(ASSET).safeTransferFrom(from, to, assets);
 
         emit Deposit(from, assets, shares);
     }
 
-    function withdraw(
-        uint shares
-    ) internal ReentrancyGuard returns (uint assets) {
-        if (shares == 0) revert ZeroShares();
+    function withdraw(uint shares) public nonReentrant returns (uint assets) {
+        assembly {
+            if iszero(shares) {
+                mstore(0x00, 0x234abd3f)
+                revert(0x1c, 0x04)
+            }
+        }
         address from = msg.sender;
 
         uint userShares = sharesOf[from];
@@ -102,26 +132,49 @@ contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
 
         uint256 totalAsset = getAssetBalance();
 
-        assets = _convertToAssets(shares, totalAsset, totalShares);
+        assets = _convertToAssets(shares);
         if (assets == 0) revert ZeroAssets();
 
         sharesOf[from] = userShares - shares;
         totalShares -= shares;
 
-        assets.SafeTransferFrom(from, assets);
+        address(ASSET).safeTransfer(from, assets);
 
         emit Withdraw(from, shares, assets);
     }
 
-    function withdrawAll() external returns (uint assets) {
+    function withdrawAll() external onlyOwner returns (uint assets) {
         uint allShares = sharesOf[msg.sender];
 
         assets = withdraw(allShares);
     }
 
-    function previewDeposit(uint assets) external view {}
+    function previewDeposit(uint assets) external view returns(uint) {
+      return _convertToShares(assets);
+    }
 
-    function reviewWithdraw(uint shares) external view {}
+    function reviewWithdraw(uint shares) external view returns(uint) {
+      return _convertToAssets(shares);
+    }
+
+    function addReward(uint amount) external onlyOwner nonReentrant {
+      assembly {
+        if iszero(amount) {
+          mstore(0x00, 0x36dbd485)
+          revert(0x1c, 0x04)
+        }
+
+        let  _totalShares := sload(totalShares.slot)
+        if iszero(_totalShares) {
+          mstore(0x00, 0x234abd3f)
+          revert(0x1c, 0x04)
+        }
+      }
+
+      address(ASSET).safeTransferFrom(msg.sender, address(this), amount);
+
+      emit RewardAdded(amount);
+    }
 
     receive() external payable {}
 
--- day03-solidity/test/Vault.t.sol
+++ day03-solidity/test/Vault.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
+import "forge-std/console2.sol";
 import {Vault} from "../src/Vault.sol";
 import "@solady/src/tokens/ERC20.sol";
 
@@ -9,19 +10,23 @@ contract VaultTestHelper is Vault {
     constructor() Vault(ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48)) {}
 
     function convertToSharesExt(
-        uint256 assets
-    ) external view returns (uint256) {
+        uint assets
+    ) external view returns (uint) {
         return _convertToShares(assets);
     }
 
     function convertToAssetsExt(
-        uint256 shares
-    ) external view returns (uint256) {
+        uint shares
+    ) external view returns (uint) {
         return _convertToAssets(shares);
     }
 
-    function getAssetDecimalsExt() external view returns (uint256) {
+    function getAssetDecimalsExt() external view returns (uint) {
         return getAssetDecimals();
+    }
+
+    function _getAssetBalance() public view returns(uint) {
+      return getAssetBalance();
     }
 }
 
@@ -29,10 +34,12 @@ contract VaultTest is Test {
     Vault public vault;
     VaultTestHelper public helper;
     ERC20 USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address Alice;
 
     function setUp() public {
         vault = new Vault(USDC);
         helper = new VaultTestHelper();
+        Alice = makeAddr("Alice");
     }
 
     function test_FreeMoney() public {
@@ -52,5 +59,41 @@ contract VaultTest is Test {
     function test_getDecimals() public {
         uint256 decimals = helper.getAssetDecimalsExt();
         assertEq(decimals, 6);
+    }
+
+
+    function test_deposit() public {
+      uint  depositAmount = 1000;
+      uint  rewardAmount = 100;
+
+      vm.startPrank(Alice);
+
+
+      vault.deposit(depositAmount);
+
+
+      (uint assetsAlice, uint sharesAlice) = vault.assetOf(Alice);
+      console2.log("After deposit - shares:", sharesAlice);
+      console2.log("After deposit - assets:", assetsAlice);
+      console2.log("Contract asset balance:", helper._getAssetBalance());
+
+      vault.addReward(rewardAmount);
+
+
+      (assetsAlice, sharesAlice) = vault.assetOf(Alice);
+      console2.log("After reward - shares:", sharesAlice);
+      console2.log("After reward - assets:", assetsAlice);
+      console2.log("Contract asset balance:", helper._getAssetBalance());
+
+      vault.withdrawAll();
+
+      (assetsAlice, sharesAlice) = vault.assetOf(Alice);
+      console2.log("After withdrawAll - shares:", sharesAlice);
+      console2.log("After withdrawAll - assets:", assetsAlice);
+
+      assertEq(sharesAlice, 0);
+      assertEq(assetsAlice, 0);
+
+      vm.stopPrank();
     }
 }
