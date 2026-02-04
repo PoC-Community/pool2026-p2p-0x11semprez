87e1b5382107c1d04d07b90973b5cd4bf07534ef
0x11semprez <traoresemprez@icloud.com>
2026-02-04 05:20:03 +0100

    feat: test ok

--- day03-solidity/src/Vault.sol
+++ day03-solidity/src/Vault.sol
@@ -3,8 +3,20 @@ pragma solidity ^0.8.3;
 
 import "@solady/src/tokens/ERC20.sol";
 import "@solady/src/utils/SafeTransferLib.sol";
+import "@solady/src/utils/ReentrancyGuard.sol";
+import "@solady/src/access/Ownable.sol";
 
-contract Vault {
+contract Vault is SafeTransferLib, ReentrancyGuard, Ownable {
+
+
+    ZeroAmount(); //keccak256 0x1f2a2005cb66a8e145327e8814e243a0996aec9bfe1e15a495778b1236dbd485
+    InsufficientShares(); //keccak26 0x399965675cfec4301cbe5ec24fb407575c5a7e4f40d219532068c8e5b35040f9
+    ZeroShares(); //keccak256 0x9811e0c7eeca2ee0fa751e2e19d78682ad689ea04816cfe0cf1c5863234abd3f
+
+
+    event Deposit(address indexed user, uint asset, uint shares);
+    event Withdraw(address indexed user, uint256 assets, uint256 shares);
+
     using SafeTransferLib for address;
 
     ERC20 private immutable ASSET;
@@ -16,7 +28,7 @@ contract Vault {
         ASSET = _asset;
     }
 
-    function getAsset() external view returns (address) {
+    function getAsset() external viors forew returns (address) {
         return address(ASSET);
     }
 
@@ -37,7 +49,9 @@ contract Vault {
         }
     }
 
-    function _convertToShares(uint256 asset) internal view returns (uint256 shares) {
+    function _convertToShares(
+        uint256 asset
+    ) internal view returns (uint256 shares) {
         uint256 _totalAssets = getAssetBalance();
 
         if (totalShares == 0) {
@@ -47,7 +61,9 @@ contract Vault {
         return (asset * totalShares) / _totalAssets;
     }
 
-    function _convertToAssets(uint256 shares) internal view returns (uint256 asset) {
+    function _convertToAssets(
+        uint256 shares
+    ) internal view returns (uint256 asset) {
         uint256 _totalAssets = getAssetBalance();
 
         if (totalShares == 0) {
@@ -56,6 +72,43 @@ contract Vault {
 
         return (shares * _totalAssets) / totalShares;
     }
+
+    function deposit(uint shares, uint assets) external ReentrancyGuard {
+        assembly {
+            if ls(assets, 0) {
+                mstore(0x00, 0x36dbd485)
+                revert(0x1c, 0x04)
+            }
+
+            let shares := 
+        }
+        uint shares = _convertToShares(assets);
+
+
+    }
+
+    //
+    Verify assets > 0
+    Calculate shares using _convertToShares(assets)
+    Verify shares > 0
+
+E - Effects:
+
+    Add shares to sharesOf[msg.sender]
+    Add shares to totalShares
+
+I - Interactions:
+
+    Transfer tokens FROM user TO vault using safeTransferFrom
+    Emit Deposit event
+
+
+    function withdrawAll(uint assets) external returns(bool) {}
+
+    function previewDeposit(uint assets) external view {}
+
+    function previewWithdraw(uint shares) external view {}
+
 
     receive() external payable {}
 
